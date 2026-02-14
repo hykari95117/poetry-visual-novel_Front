ededebc7d22a5bf426f1ee4b773e1498fdc5ce60
hykari95117-tech <hykari95117@gmail.com>
2026-02-14 18:35:32 +0900

    opening scene

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-background">
+      <h1 className="game-title">Geum-soon</h1>
+      <div className="select-box-group">
+        <button className="select-box">START</button>
+        <button className="select-box">SAVED</button>
+        <button className="select-box">EXIT</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
