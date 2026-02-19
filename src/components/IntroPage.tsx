9cd655f73de45e493ee63bc5af74bf3dc1e53d54
hykari95117-tech <hykari95117@gmail.com>
2026-02-19 21:45:13 +0900

    window에 걸린 onclick, onKeydown 이벤트 해제, useNavigate로 변경

--- src/components/IntroPage.tsx
+++ src/components/IntroPage.tsx
@@ -1,23 +1,27 @@
 import '../styles/IntroPage.css'
+import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const IntroPage = () => {
     const navigate = useNavigate();
-    const goToOpening = () => {
-        // 키 입력은 Enter || space bar 입력 시 넘어간다.
-        window.onkeydown = (e) => {
-            if(e.key === 'Enter' || e.key === ' ') {
-                navigate('/opening');
-            }
-        }
-        // 마우스 클릭
-        window.onclick = () => {
+
+    const handleClick = () => {
+        navigate('/opening');
+    }
+
+    const handleKeyDown = (e: React.KeyboardEvent | KeyboardEvent) => {
+        if (e.key === 'Enter' || e.key === ' ') {
             navigate('/opening');
         }
     }
 
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+        return () => window.removeEventListener('keydown', handleKeyDown);
+    }, []);
+
     return (
-        <div className="click-to-start" onClick={goToOpening} onKeyDown={goToOpening}>
+        <div className="click-to-start" onClick={handleClick}>
             <p className="click-text">CLICK TO START</p>
         </div>
     )
